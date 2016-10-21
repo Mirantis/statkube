3e478d5f4596b530f45b550784254ced0a4b6ba9
Maciej Kwiek <maciej.iai@gmail.com>
2016-10-21 12:27:15 +0200

    Company pie charts
    Company data is retrieved properly from db
    Pie charts with companies contribution added

--- models/models.go
+++ models/models.go
@@ -45,6 +45,7 @@ type PullRequest struct {
 	WorkPeriodId sql.NullInt64
 	Developer    Developer
 	DeveloperId  uint
+	MergedAt     *time.Time
 }
 
 func Migrate(db *gorm.DB) {
@@ -63,8 +64,13 @@ type DevStats struct {
 }
 
 func GetDevStats(db *gorm.DB) ([]DevStats, error) {
+	//TODO: add time logic from GetCompanyStats
 	var developers []DevStats
-	rows, err := db.Table("developers").Select("developers.full_name, COUNT(prs.developer_id)").Joins("left join pull_requests prs on prs.developer_id = developers.id").Group("prs.developer_id").Rows()
+	rows, err := db.Table("developers").
+		Select("developers.full_name, COUNT(prs.developer_id)").
+		Joins("left join pull_requests prs on prs.developer_id = developers.id").
+		Group("prs.developer_id").
+		Rows()
 	if err != nil {
 		return nil, err
 	}
@@ -79,4 +85,31 @@ func GetDevStats(db *gorm.DB) ([]DevStats, error) {
 	}
 
 	return developers, nil
+}
+
+func GetCompanyStats(db *gorm.DB, start, end time.Time) ([]DevStats, error) {
+	var stats []DevStats
+
+	rows, err := db.Table("companies").
+		Select("companies.name, COUNT(pull_requests.id)").
+		Joins("join work_periods on work_periods.company_id=companies.id").
+		Where("1=1"). //filter by start and end date when it's available
+		Joins("left join pull_requests on pull_requests.work_period_id=work_periods.id").
+		Group("companies.id").
+		Rows()
+
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var name string
+		var count uint
+		err := rows.Scan(&name, &count)
+		if err != nil {
+			return nil, err
+		}
+		stats = append(stats, DevStats{name, count})
+	}
+
+	return stats, nil
 }
